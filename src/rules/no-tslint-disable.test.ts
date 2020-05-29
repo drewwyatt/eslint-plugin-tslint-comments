a9758fee544f546ff0f713180a78330a7ab44731
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-05-28 22:17:11 -0400

    update test cases

--- src/rules/no-tslint-disable.test.ts
+++ src/rules/no-tslint-disable.test.ts
@@ -1,6 +1,16 @@
 import rule from './no-tslint-disable'
 import { RuleTester } from '../rule-tester'
 
+const PALANTIR_EXAMPLES = [
+  '/* tslint:disable */', // Disable all rules for the rest of the file
+  '/* tslint:enable */', // Enable all rules for the rest of the file
+  '/* tslint:disable:rule1 rule2 rule3... */', // Disable the listed rules for the rest of the file
+  '/* tslint:enable:rule1 rule2 rule3... */', // Enable the listed rules for the rest of the file
+  '// tslint:disable-next-line', // Disables all rules for the following line
+  'someCode(); // tslint:disable-line', // Disables all rules for the current line
+  '// tslint:disable-next-line:rule1 rule2 rule3...', // Disables the listed rules for the next line
+]
+
 const ruleTester = new RuleTester({
   parser: '@typescript-eslint/parser',
 })
@@ -13,19 +23,28 @@ ruleTester.run('no-tslint-disable', rule, {
     {
       code: 'let a = new Array();',
     },
+    {
+      code: '// some other comment',
+    },
+    {
+      code: '// TODO: this is a comment that mentions tslint',
+    },
+    {
+      code: '/* another comment that mentions tslint */',
+    },
   ],
   invalid: [
-    {
-      code: '/* tslint:disable */',
-      output: '/* tslint:disable */',
+    ...PALANTIR_EXAMPLES.map(ex => ({
+      code: ex,
+      output: ex,
       errors: [
         {
-          messageId: 'commentDetected',
+          messageId: 'commentDetected' as const,
           data: { type: 'string' },
           line: 1,
           column: 1,
         },
       ],
-    },
+    })),
   ],
 })
