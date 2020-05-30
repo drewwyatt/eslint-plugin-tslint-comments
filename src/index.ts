4da82ff3d27acc8268e812ce01f6045ae69efe07
Drew Wyatt <drew.j.wyatt@gmail.com>
2020-05-30 01:02:45 -0400

    add configs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import forbidAll from './rules/forbid-all'
 
+export const configs = {
+  recommended: {
+    plugins: ['tslint-comments'],
+    rules: {
+      'tslint-comments/forbid-all': 'error',
+    },
+  },
+}
+
 export const rules = {
   'forbid-all': forbidAll,
 }
